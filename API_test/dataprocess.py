281cf9cc3df47cafdca792b4353013448bef8e03
PBustamante65 <136853530+PBustamante65@users.noreply.github.com>
2024-10-14 15:56:43 -0600

    Created new classes

---
+++ API_test/dataprocess.py
@@ -0,0 +1,451 @@
+import pandas as pd
+import json
+import ast
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import ShuffleSplit
+from sklearn.impute import SimpleImputer
+from pandas.plotting import scatter_matrix
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from joblib import dump, load
+import os
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score, f1_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import classification_report
+from sklearn.model_selection import learning_curve
+from sklearn.utils import resample
+from imblearn.over_sampling import SMOTE
+from sklearn import svm
+from sklearn.svm import SVC
+import pickle as pkl
+import ast
+
+
+
+class append:
+    def __init__(self,df2):
+
+        self.df = df2 
+
+    def name(self, data):
+
+        t1 = ''
+        for i in range(0,8):
+            t1 += data[i]
+        parts2 = t1.str.split(':')
+        t2 = parts2.str[6]
+        parts3 = t2.str.split('\'')
+        name = parts3.str[1]
+        return name
+    
+
+    def designation(self, data):
+
+        specific = 'name_limited'
+        contains_string = self.parts1.apply(lambda x: specific in x[4] if len(x) > 4 else False)
+
+        if contains_string.any():
+            name_limited = data[4]
+            designation = data[5]
+            nasa_jpl_url = data[6]
+            abs_magnitude_h = data[7]
+            diameter = ' '
+            for i in range(8,16):
+                diameter += data[i] + ','
+            hazard = data[16]
+        else:
+            name_limited = ' '
+            designation = data[4]
+            nasa_jpl_url = data[5]
+            abs_magnitude_h = data[6]
+            diameter = ' '
+            for i in range(7,15):
+                diameter += data[i] + ','
+            hazard = data[15]
+
+
+        return name_limited, designation, nasa_jpl_url, abs_magnitude_h, diameter, hazard
+    
+    def orbit(self, data):
+
+        orbit = ' '
+        orbit = data[1]
+        parts = orbit.str.split(',')
+        orbit_data1 = ' '
+        for i in range(1,26):
+            orbit_data1 += parts.str[i].astype(str) + ','
+        orbit_data1 = orbit_data1.str.rstrip(',')
+        parts2 = orbit_data1.str.split(':')
+        orbit_data = ' '
+        for i in range(1,30):
+            orbit_data += parts2.str[i].astype(str) + ':'
+        orbit_data = orbit_data.str.rstrip(':')
+        #orbit_data = orbit_data.apply(ast.literal_eval)
+
+        is_sentry_object1 = ' '
+        is_sentry_object1 = parts.str[26]
+        parts1 = is_sentry_object1.str.split(':')
+        is_sentry_object = parts1.str[1].astype(str)
+        is_sentry_object = is_sentry_object.str.rstrip('}')
+        is_sentry_object = is_sentry_object.str.lower() == 'true'
+        is_sentry_object = is_sentry_object.astype(bool)
+
+        return orbit_data, is_sentry_object
+
+    def separation(self):
+    
+
+        # Assuming df2 is already defined
+        df_result = pd.DataFrame()  # Initialize an empty DataFrame to store results
+
+        for i in range(len(self.df)):
+            df2t = self.df.iloc[[i]]  # Process each row
+            df2t = pd.DataFrame(df2t)
+            df2t['data'] = df2t['neo_data']
+            df2t = df2t.drop(columns=['neo_data'])
+            df2t = df2t.astype(str)
+
+            parts = df2t['data'].str.split(' ')
+            parts1 = df2t['data'].str.split(',')
+            partsorbit = df2t['data'].str.split(']')
+            self.parts1 = parts1
+            name_limited, designation, nasa_jpl_url, abs_magnitude_h, diameter, hazard = self.designation(parts1.str)
+            orbit_data, is_sentry_object = self.orbit(partsorbit.str)
+            
+            df2t['links'] = parts.str[1] + parts.str[2]
+            df2t['id'] = parts.str[4]
+            df2t['id'] = df2t['id'].str.replace('\'', '')
+            df2t['id'] = df2t['id'].str.replace(',', '')
+            df2t['id'] = df2t['id'].astype(int)
+
+
+            df2t['neo_reference_id'] = parts.str[6]
+            df2t['neo_reference_id'] = df2t['neo_reference_id'].str.replace('\'', '')
+            df2t['neo_reference_id'] = df2t['neo_reference_id'].str.replace(',', '')
+            df2t['neo_reference_id'] = df2t['neo_reference_id'].astype(int)
+
+
+            df2t['name'] = self.name(parts1.str)
+
+
+            df2t['name_limited'] = name_limited
+            parts = df2t['name_limited'].str.split(' ')
+            df2t['name_limited'] = parts.str[2]
+            df2t['name_limited'] = df2t['name_limited'].astype(str)
+            df2t['name_limited'] = df2t['name_limited'].str.replace('\'', '')
+
+
+            df2t['designation'] = designation
+            parts = df2t['designation'].str.split(' ')
+            df2t['designation'] = parts.str[2]
+            df2t['designation'] = df2t['designation'].astype(str)
+            df2t['designation'] = df2t['designation'].str.replace('\'', '')
+
+
+            df2t['nasa_jpl_url'] = nasa_jpl_url
+            parts = df2t['nasa_jpl_url'].str.split(' ')
+            df2t['nasa_jpl_url'] = parts.str[2]
+            df2t['nasa_jpl_url'] = df2t['nasa_jpl_url'].astype(str)
+            df2t['nasa_jpl_url'] = df2t['nasa_jpl_url'].str.replace('\'', '')
+
+
+            df2t['absolute_magnitude_h'] = abs_magnitude_h
+            parts = df2t['absolute_magnitude_h'].str.split(' ')
+            df2t['absolute_magnitude_h'] = parts.str[2]
+            df2t['absolute_magnitude_h'] = df2t['absolute_magnitude_h'].astype(str)
+            df2t['absolute_magnitude_h'] = df2t['absolute_magnitude_h'].str.replace('\'', '')
+            df2t['absolute_magnitude_h'] = df2t['absolute_magnitude_h'].astype(float)
+
+
+            df2t['estiated_diameter1'] = diameter
+            df2t['estiated_diameter1'] = df2t['estiated_diameter1'].str.rstrip(',')
+            parts = df2t['estiated_diameter1'].str.split(' ')
+
+            df2t['estimated_diameter'] = ' '
+            for i in range(3,23):
+                df2t['estimated_diameter'] += parts.str[i] + ' '
+            df2t.drop(columns=['estiated_diameter1'], inplace=True)
+
+
+            df2t['is_potentially_hazardous_asteroid'] = hazard
+            parts = df2t['is_potentially_hazardous_asteroid'].str.split(' ')
+            df2t['is_potentially_hazardous_asteroid'] = parts.str[2]
+            df2t['is_potentially_hazardous_asteroid'] = df2t['is_potentially_hazardous_asteroid'].astype(str)
+            df2t['is_potentially_hazardous_asteroid'] = df2t['is_potentially_hazardous_asteroid'].str.lower() == 'true'
+            df2t['is_potentially_hazardous_asteroid'] = df2t['is_potentially_hazardous_asteroid'].astype(bool)
+
+            parts1 = df2t['data'].str.split('[')
+            df2t['approachdata'] = ' '
+            df2t['approachdata'] = parts1.str[1]
+            parts2 = df2t['approachdata'].str.split(']')
+            df2t['close_approach_data'] = ' '
+            df2t['close_approach_data'] = parts2.str[0]
+            df2t['close_approach_data'] = df2t['close_approach_data'].apply(lambda x: f'[{x}]')
+            df2t['close_approach_data'] = df2t['close_approach_data'].apply(ast.literal_eval)
+            # df2t['close_approach_data'] = df2t['close_approach_data'].apply(lambda x: ' ' if x == [] else x)
+            df2t = df2t[df2t['close_approach_data'].apply(lambda x: x != [])]
+
+            df2t.drop(columns=['approachdata'], inplace=True)
+
+
+            # df2t['close_approach_data'] = df2t['close_approach_data'].apply(ast.literal_eval)
+
+
+
+            df2t['orbital_data'] = orbit_data
+
+
+            df2t['is_sentry_object'] = is_sentry_object
+            
+                        
+
+
+            df_result = pd.concat([df_result, df2t], ignore_index=True)  # Append to the result DataFrame
+
+        df_result = df_result.drop(columns=['data'])
+        df_result.dropna(subset=['close_approach_data'], inplace=True)
+        self.df_result = df_result
+
+        return self.df_result
+    
+    def concat(self):
+
+        df_result = self.separation()
+
+        df = pd.read_csv('/Volumes/Maestria/GitHub/NEO/NEO/API_test/neo_data.csv')
+
+        df = pd.concat([df, df_result], ignore_index=True)
+
+        return df
+    
+class OverallProcessor:
+
+    def __init__(self, df):
+        self.df = df
+
+    def explode(self):
+
+        def explode_approach(self):
+            self.df['close_approach_data'] = self.df['close_approach_data'].apply(ast.literal_eval)
+            self.df = self.df.explode("close_approach_data").reset_index(drop=True)
+            normalized_close_approach_data = pd.json_normalize(self.df['close_approach_data'])
+            self.df = pd.concat([self.df.drop(columns=['close_approach_data']), normalized_close_approach_data], axis=1)
+            
+        
+        def clean_diameter(self):
+                self.df.drop(columns=['neo_reference_id', 'name_limited', 'links', 'nasa_jpl_url'], inplace=True)
+                self.df['estimated_diameter'] = self.df['estimated_diameter'].str.replace('\'', '')
+                self.df['estimated_diameter'] = self.df['estimated_diameter'].str.replace('{', '')
+                self.df['estimated_diameter'] = self.df['estimated_diameter'].str.replace('}', '')
+                parts = self.df['estimated_diameter'].str.split(',')
+                self.df['estimated_diameter'] = parts.str[0] + parts.str[1]
+                parts = self.df['estimated_diameter'].str.split(':')
+                self.df['estimated_diameter'] = parts.str[1] + parts.str[2] + parts.str[3] 
+                parts = self.df['estimated_diameter'].str.split(' ')
+                self.df['estimated_diameter_min'] = parts.str[2]
+                self.df['estimated_diameter_max'] = parts.str[4]
+                self.df.drop(columns=['estimated_diameter'], inplace=True)
+
+
+        def clean_orbits(self):
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace('\'', '')
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace('{', '')
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace('}', '')
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace(']', '')
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace('[', '')
+            parts = self.df['orbital_data'].str.split(',')
+            self.df['extracted_orbital_data'] = parts.str[7]+parts.str[10]+parts.str[12]+parts.str[15]+parts.str[17]
+            parts = self.df['extracted_orbital_data'].str.split(' ')
+            self.df['minimum_orbit_intersection'] = parts.str[2]
+            self.df['eccentricity'] = parts.str[4]
+            self.df['inclination'] = parts.str[6]
+            self.df['perihilion_distance'] = parts.str[8]
+            self.df['aphelion_distance'] = parts.str[10]
+            self.df.drop(columns=['orbital_data', 'extracted_orbital_data'], inplace=True)    
+
+        explode_approach(self)
+        clean_diameter(self)
+        clean_orbits(self)
+        return self.df
+    
+    def clean(self):
+
+        def explode_approach(self):
+            self.df['close_approach_data'] = self.df['close_approach_data'].apply(ast.literal_eval)
+            self.df = self.df.explode("close_approach_data").reset_index(drop=True)
+            normalized_close_approach_data = pd.json_normalize(self.df['close_approach_data'])
+            self.df = pd.concat([self.df.drop(columns=['close_approach_data']), normalized_close_approach_data], axis=1)
+            
+        
+        def clean_diameter(self):
+                self.df.drop(columns=['neo_reference_id', 'name_limited', 'links', 'nasa_jpl_url'], inplace=True)
+                self.df['estimated_diameter'] = self.df['estimated_diameter'].str.replace('\'', '')
+                self.df['estimated_diameter'] = self.df['estimated_diameter'].str.replace('{', '')
+                self.df['estimated_diameter'] = self.df['estimated_diameter'].str.replace('}', '')
+                parts = self.df['estimated_diameter'].str.split(',')
+                self.df['estimated_diameter'] = parts.str[0] + parts.str[1]
+                parts = self.df['estimated_diameter'].str.split(':')
+                self.df['estimated_diameter'] = parts.str[1] + parts.str[2] + parts.str[3] 
+                parts = self.df['estimated_diameter'].str.split(' ')
+                self.df['estimated_diameter_min'] = parts.str[2]
+                self.df['estimated_diameter_max'] = parts.str[4]
+                self.df.drop(columns=['estimated_diameter'], inplace=True)
+
+
+        def clean_orbits(self):
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace('\'', '')
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace('{', '')
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace('}', '')
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace(']', '')
+            self.df['orbital_data'] = self.df['orbital_data'].str.replace('[', '')
+            parts = self.df['orbital_data'].str.split(',')
+            self.df['extracted_orbital_data'] = parts.str[7]+parts.str[10]+parts.str[12]+parts.str[15]+parts.str[17]
+            parts = self.df['extracted_orbital_data'].str.split(' ')
+            self.df['minimum_orbit_intersection'] = parts.str[2]
+            self.df['eccentricity'] = parts.str[4]
+            self.df['inclination'] = parts.str[6]
+            self.df['perihilion_distance'] = parts.str[8]
+            self.df['aphelion_distance'] = parts.str[10]
+            self.df.drop(columns=['orbital_data', 'extracted_orbital_data'], inplace=True)   
+
+
+        def clean_df(self):
+            self.df.drop(columns=['id', 'name', 'designation', 'is_sentry_object', 'close_approach_date', 'close_approach_date_full', 'epoch_date_close_approach', 'orbiting_body', 'relative_velocity.kilometers_per_second', 'relative_velocity.miles_per_hour', 'miss_distance.astronomical', 'miss_distance.lunar', 'miss_distance.miles' ], inplace=True)
+            self.df = self.df.rename(columns={'is_potentially_hazardous_asteroid': 'is_hazardous'}) 
+            estimated_diameter_average = (self.df['estimated_diameter_min'].astype(float) + self.df['estimated_diameter_max'].astype(float)) / 2
+            self.df['estimated_diameter_average'] = estimated_diameter_average
+
+        def encoder(self):
+            le = LabelEncoder()
+            self.df['is_hazardous'] = le.fit_transform(self.df['is_hazardous'])
+
+        explode_approach(self)
+        clean_diameter(self)
+        clean_orbits(self)
+        clean_df(self)
+        encoder(self)
+        return self.df  
+    
+    def smote(self):
+             
+        self.df_test = self.df.copy()
+
+        sm = SMOTE(sampling_strategy='minority', random_state=42)
+        oversampled_X, oversampled_Y = sm.fit_resample(self.df_test.drop('is_hazardous', axis=1), self.df_test['is_hazardous'])
+        self.df = pd.concat([pd.DataFrame(oversampled_Y), pd.DataFrame(oversampled_X)], axis=1)
+        return self.df
+    
+
+class scalesplit:
+    def __init__(self, df):
+        self.df = df
+
+    def ssplit(self):
+
+        def split(self):
+
+         X = self.df.drop(columns=['is_hazardous'])
+         y = self.df['is_hazardous']
+
+
+         split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+         for train_index, test_index in split.split(X, y):
+          self.X_train, self.X_test = X.loc[train_index], X.loc[test_index]
+          self.y_train, self.y_test = y.loc[train_index], y.loc[test_index]
+
+         
+
+        def scale(self):
+         
+            self.X_train = StandardScaler().fit_transform(self.X_train)
+            self.X_test = StandardScaler().fit_transform(self.X_test)
+
+    
+        split(self)
+        scale(self)
+
+        X_train = self.X_train
+        X_test = self.X_test
+        y_train = self.y_train
+        y_test = self.y_test
+
+        return X_train, X_test, y_train, y_test
+
+class LogRegression:
+    def __init__(self, X_train, X_test, y_train, y_test):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+    def Regression(self):
+
+            def gridsearch():
+                logReg = LogisticRegression()
+
+                param_grid = {'solver': ['liblinear', 'newton-cholesky'],
+              'penalty':['none', 'l2'],
+              'C':[0.001, 0.01, 0.1, 1, 10, 100],
+              'n_jobs': [8],
+              'random_state': [0, 42, 32],
+              'fit_intercept': [True, False],
+              'warm_start': [True, False]
+}
+
+
+                grid_search = GridSearchCV(logReg, param_grid, cv=5, verbose=0, n_jobs=-1)
+                grid_search.fit(self.X_train, self.y_train)
+
+
+                self.best_estimator_ = LogisticRegression(C=0.001, fit_intercept=False, n_jobs=8, random_state=0,solver='liblinear', warm_start=True) 
+
+
+                print(f'Best parameters: {grid_search.best_params_}')
+                print(f'Best Score: {grid_search.best_score_}')
+                print(f'Best Estimator: {grid_search.best_estimator_} ')
+
+            def fit(self):
+
+                best_model = self.best_estimator_
+                best_model.fit(self.X_train, self.y_train)
+
+                prediction = best_model.predict(self.X_test)
+
+                accuracy = accuracy_score(self.y_test, prediction)
+                recall = recall_score(prediction, self.y_test)
+                f1 = f1_score(prediction, self.y_test)
+
+                print (f'The accuracy score is {accuracy}, The recall score is {recall}, The f1 score is {f1}')
+
+                print(f'Classification Report: \n {classification_report(self.y_test, prediction)}')
+
+                cm = confusion_matrix(self.y_test, prediction)
+                disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+                disp.plot(cmap='Blues')
+                plt.show()
+
+                cm2 = cm / cm.sum(axis=1)[:, np.newaxis]
+
+                sns.heatmap(cm2, annot=True, cmap='Blues')
+                
+            gridsearch()
+            fit(self)
+    
